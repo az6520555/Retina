efd2c7a10b6342bc4a0f03f4b8031dd5ce538caa
az6520555 <az6520555@gmail.com>
2022-09-03 00:25:07 +0800

    20220903 added

--- transform_rawdata/rename_files_from_parameter.m
+++ transform_rawdata/rename_files_from_parameter.m
@@ -1,14 +1,15 @@
-
 clear
-newname_path='\\192.168.0.102\Public\Retina\Chou\Exp\20220823\23-Aug-2022'; % change every time
-cd(newname_path)
-all_file = dir('*.mat') ; % change the type of the files which you want to select, subdir or dir.
-n_file = length(all_file);
-for i =1:n_file;date{i,1}=all_file(i).date;end
-[datesort ind]=sort(date)
+filename_mat_path='\\192.168.0.102\Public\Retina\Chou\stimulus_saving\01-Sep-2022';
+load(fullfile(filename_mat_path,'stimuli_seq_01-Sep-2022_17-0.mat'))
 
-oldname_path='\\192.168.0.102\Public\Retina\Chou\Exp\20220823\SplitData'  % change every time
+oldname_path='\\192.168.0.102\Public\Retina\Chou\Exp\20220823\SplitData';  % change every time
 cd(oldname_path)
-for i=1:n_file
-    movefile([num2str(i),'.mat'],all_file(ind(i)).name)
+all_file=dir('*.mat');
+n_file=length(all_file);
+if n_file==size(parameters,1)
+    for i=1:n_file
+        movefile([num2str(i),'.mat'],all_file(ind(i)).name)
+    end
+else
+    'error with number of files';
 end
