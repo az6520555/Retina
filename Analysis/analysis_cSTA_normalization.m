1dc7752acaab7673e5341a9ca319c92c5753146b
az6520555 <az6520555@gmail.com>
2022-09-27 20:28:42 +0800

    20220927

--- Analysis/analysis_cSTA_normalization.m
+++ Analysis/analysis_cSTA_normalization.m
@@ -17,7 +17,7 @@ rr =    [9,17,25,33,41,49,...
       7,15,23,31,39,47,55,63,...
         16,24,32,40,48,56];
 roi = [1:60];
-file_numbers=[16 18 17 19 20]; % file selections 16 18 17 19 20;31 33 32 34 35;21 23 22 29 30
+file_numbers=[25:29]; % file selections 16 18 17 19 20;31 33 32 34 35;21 23 22 29 30
 
 colors_default={[0, 0.4470, 0.7410],[0.8500, 0.3250, 0.0980],[0.9290, 0.6940, 0.1250],[0.4940, 0.1840, 0.5560], ...
     [0.4660, 0.6740, 0.1880],[0.3010, 0.7450, 0.9330],[0.6350, 0.0780, 0.1840]};
@@ -30,7 +30,7 @@ mkdir MIandSTA
 % for i_set = 1:size(file_numbers,1)
 for z = 1:size(file_numbers,2)
     clearvars -except all_file n_file z SamplingRate cc ey isi2 statispks statistime w fr...
-        information rr STA roi fignames date datapath Date colors_default file_numbers ls_set i_set
+        information rr STA roi fignames date datapath Date colors_default file_numbers ls_set i_set BinningSpike_seq
     file = all_file(file_numbers(z)).name;
     [pathstr, name, ext] = fileparts(file);
     directory = [pathstr,'\'];
@@ -52,69 +52,14 @@ for z = 1:size(file_numbers,2)
     
     % transform stimulus from volt to intensity
     inten=(inten-32768).*125*10^(-6); % transfrom from raw data of MCRack to output volt
-    
-    % load calibration data
-%     load('\\nasd32ad0\Experiment\Retina\Chou\stimulus saving\19-Apr-2020\calibration\calibration_PAC_19-Apr-2020.mat')
-%     inten=inten-offset;
-%     Ip=inten/10.421/10^6;
-%     r=0.37;
-%     P=Ip/r;
-%     A=13*10^-6;
-%     inten=P/A*1000; % unit: mW/m^2
-%% substitute by sorted spikes
-%     ss=[29,30,28,27,22,21,14,20,...
-%         13,6,12,5,19,11,4,3,10,...
-%         18,2,9,1,8,17,7,16,15,26,...
-%         25,23,24,32,31,33,34,39,...
-%         40,47,41,48,55,49,56,42,...
-%         50,57,58,51,43,59,52,60,...
-%         53,44,54,45,46,35,36,38,37];
-%     Spikes = cell(1,60);
-%     
-%     load([path_sort,filename(1:end-4),'_sort.mat']);
-%     temp_spikes={};
-%     for h=1:60
-%         if h<11
-%             temp_spikes{ss(h)} = eval(['adc00',int2str(h-1)]);
-%         else
-%             temp_spikes{ss(h)} = eval(['adc0',int2str(h-1)]);
-%         end
-%     end
-%     for i=1:60
-%         if isempty(temp_spikes{i})==1
-%             continue
-%         end
-%         for j=1:length(temp_spikes{i}(:,1))
-%             if temp_spikes{i}(j,3)==1
-%                 Spikes{i}=[Spikes{i} temp_spikes{i}(j,1)];
-%             end
-%         end
-%     end
-
-%================= for sorted data in .xlsx files =========================
-%     ss = [29,18,39,53,4,31,59,5,23,38,13,15,...
-%         42,37,21,17,55,35,28,9,47,54,10,34,...
-%         60,11,32,43,12,25,57,20,16,56,38,22,...
-%         8,48,46,30,2,40,44,3,33,52,19,24,...
-%         51,6,26,50,14,7,49,36,27,1,41,45];
-%     Spikes = {};
-%     xls = xlsread([filename(1:end-4),'.xls']);
-%     for j = 1:max(xls(:,1))
-%             temp = 0;
-%         for i = 1:length(xls)
-%             if xls(i,1) == j
-%                 temp = temp+1;
-%                 Spikes{ss(j)}(1,temp) = xls(i,2);
-%             end
-%         end
-%     end
-%     BinningTime = [TimeStamps(1) : BinningInterval : TimeStamps(end)];
-%     BinningSpike = zeros(60,length(BinningTime));
-%     for i = 1:60
-%         [n,xout] = hist(Spikes{i},BinningTime) ;
-%         BinningSpike(i,:) = n ;
-%     end
-%==========================================================================
+%   load calibration data
+    load('\\192.168.0.102\Public\Retina\Chou\Exp\20220916\16-Sep-2022\calibration\calibration_PAC_16-Sep-2022.mat')
+    inten=inten-offset;
+    Ip=inten/10.421/10^6;
+    r=0.37;
+    P=Ip/r;
+    A=13*10^-6;
+    inten=P/A*1000; % unit: mW/m^2
    
 %% spike process
     BinningTime = [TimeStamps(1) : BinningInterval : TimeStamps(end)];
@@ -126,97 +71,64 @@ for z = 1:size(file_numbers,2)
      BinningSpike(:,1) = 0;BinningSpike(:,end) = 0;    
      temprr=0;
 
+    BinningSpike_seq(:,:,z)=BinningSpike;
+end
+
+%% superposition of spikes
+BinninSpike_sup=sum(BinningSpike_seq,3);
 %% STA'Name',fignames{z});
-    figure(1);
-    set(gcf, 'InvertHardCopy', 'off');
-    set(gcf,'units','normalized','outerposition',[0 0 1 1])
+figure(1);
+set(gcf, 'InvertHardCopy', 'off');
+set(gcf,'units','normalized','outerposition',[0 0 1 1])
 
-    for nn = 1:length(roi)
-        spike = BinningSpike(roi(nn),:);
+for nn = 1:length(roi)
+    spike = BinningSpike(roi(nn),:);
 
-        window = 2;  %STA window
-        window2 = 1;
-        sts = [];
-        temp = 0;
-        spike(1:window/BinningInterval) = 0;
-        spike(length(spike)-window2/BinningInterval-10:end) = 0;
-        inten2=inten; %-mean(inten)
-        for in = 1:length(spike)
-           if spike(in)~=0
-              temp = temp+1;
-              sts(temp,:) = spike(in)*inten2(in-round(window/BinningInterval):in+round(window2/BinningInterval));
-           end
-        end
-        STA = sum(sts)/sum(spike);
-        STA = STA/max(abs(STA));
-        STA = normalize(STA);
-        
-        t = [-window*1000:bin:window2*1000];
-        sub1=subplot(8,8,rr(nn));
-        plot(t,STA,'LineWidth',1);% ,'color',colors_default{z} ,'linestyle',ls_set{i_set}
-        title(num2str(nn))
-        xlim([-window*1000 window2*1000])
-%         ylim([-1 1])
-        hold on
-        STAAAAA{nn}=STA;
-        TimeShift{nn}=t;
+    window = 2;  %STA window
+    window2 = 0;
+    sts = [];
+    temp = 0;
+    spike(1:window/BinningInterval) = 0;
+    spike(length(spike)-window2/BinningInterval-10:end) = 0;
+    inten2=inten; %-mean(inten)
+    for in = 1:length(spike)
+       if spike(in)~=0
+          temp = temp+1;
+          sts(temp,:) = spike(in)*inten2(in-round(window/BinningInterval):in+round(window2/BinningInterval));
+       end
     end
     
-    % ======= plot single channel =============
-%     channel=16;
-%     figure(2);hold on;box on
-%     plot(t,STAAAAA{channel},'LineWidth',2,'color',colors_default{i_set})
-%     xlim([-window*1000 window2*1000])
-%     ylim([-1 1])
-%     xlabel('time (ms)')
-%     ylabel('STA')
+    STA = sum(sts,1)/sum(spike);
+
+%         STA = STA/max(abs(STA));
+%         STA = normalize(STA);
     
-% %======= derivative of  STA ===============
-%     for ii=1:60
-%         subplot(8,8,rr(ii));hold on
-%         yyaxis right
-%         if ~isnan(STAAAAA{ii})
-%             dSTA=diff(STAAAAA{ii});
-%         else
-%             dSTA=NaN;
-%         end
-%         plot(t(1:end-1),dSTA,'LineWidth',1.5)
-%     end
+    t = [-window*1000:bin:window2*1000];
+    sub1=subplot(8,8,rr(nn));
+    try
+        plot(t,STA,'LineWidth',1);% ,'color',colors_default{z} ,'linestyle',ls_set{i_set}
+    catch
+    end
+    title(num2str(nn))
+    xlim([-window*1000 window2*1000])
+%         ylim([-1 1])
+    hold on
+    STAAAAA{nn}=STA;
+    TimeShift{nn}=t;
+end
 
 
 % save([datapath,'\MIandSTA\',filename(1:end-4),'_STA.mat'],'STAAAAA','TimeShift')
-end
-
-% end
 
 
-%% Classify ON and OFF cells from cSTA
-% onoffindex=[];
-% for i =1:60
-%     try
-%         onoffindex(i)=sum(STAAAAA{1,i})/length(STAAAAA{1,i});
-%     catch
-%         continue
-%     end
+%% calculate convoluted signal and obtain nonlinear filter
+channel=16;
+kt=STAAAAA{channel};
+gt=conv(inten,kt(end:-1:1),'valid'); % (length(kt)-1) elements are dropped
+% figure;hold on
+% for i=1:10
+%     plot(gt(i:end),BinningSpike(channel,length(kt):end-i+1),'o')
 % end
-
-%%
-% %% change figure color to classify different cells
-% % === P ===
-% path_class='F:\我的雲端硬碟\Retina exp\exp data\整理\NP_classification\HMM\';
-% file_class=[Date,'_HMM_G30_sort_unit1_MI_also_other_parameters.mat'];
-% savepath='F:\我的雲端硬碟\Retina exp\exp data\整理\figures\';
-% load([path_class,file_class]);
-% figure(1)
-% for n=1:length(P_channel)
-%     subplot(8,8,rr(P_channel(n)))
-%     set(gca,'Color',[0.8 1 0.8])
-% end
-% % === N ===
-% for n=1:length(N_channel)
-%     subplot(8,8,rr(N_channel(n)))
-%     set(gca,'Color',[0.8 0.8 1])
-% end
-% 
-% % saveas(gcf,[savepath,Date,'_STA_NPclassified.fig']);
-%    
+figure;hold on
+plot(normalize(gt))
+plot(normalize(inten(length(kt):end)))
